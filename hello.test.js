1fdc5b37f9025490b0316efe25107207fb3ca359
whyang115 <whyang115@gmail.com>
2019-04-29 11:27:33 +0800

    test travis ci

--- hello.test.js
+++ hello.test.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var hello = require('./hello');
+var expect = require("chai").expect;
+var hello = require("./hello");
 
-describe('run hello.js', function() {
-  it('should return "Hello World"', function() {
-    expect(hello()).to.equal('Hello World');
+describe("run hello.js", function() {
+  it('should return "Hello node"', function() {
+    expect(hello()).to.equal("Hello node");
   });
 });
